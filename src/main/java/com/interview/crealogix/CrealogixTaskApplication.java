6262dc9d40fdbe96019a059e333a335fe22994dc
Mateusz Fislak <mfislak@valuelogic.one>
2021-10-17 12:12:27 +0200

    add sprinboot scaffold

---
+++ src/main/java/com/interview/crealogix/CrealogixTaskApplication.java
@@ -0,0 +1,13 @@
+package com.interview.crealogix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrealogixTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrealogixTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/interview/crealogix/CrealogixTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.crealogix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrealogixTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
